0ea1275449c0982ec8431e682837f7847674175d
Aras Ekinci <dev@arasekinci.com>
2021-12-18 23:34:53 +0300

    changeset(mime-type): fix descriptions and add new types
    add kind to all mime types
    
    add mime types .weba, .webm, .webp, .woff and .wofff2

--- test/mime-type.test.ts
+++ test/mime-type.test.ts
@@ -4,7 +4,7 @@ describe('Mime Type', () => {
   test('should return the equivalent of the mime type', () => {
     const mimetype = new MimeType('application/pdf')
 
-    expect(mimetype.description).toBe('Portable Document Format')
+    expect(mimetype.description).toBe('Adobe Portable Document Format')
   })
 
   test('should return the equivalent of the file extension', () => {
